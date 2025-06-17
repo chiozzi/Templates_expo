f12390e51dd3f322e1d4846573426c4c7498e8c6
chiozzi <chiozziletici0114@gmail.com>
2025-06-16 21:17:51 -0300

    atualização

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,67 @@
 import { Link } from "expo-router";
 import { View } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-    <Link href="/ex01">Layout Básico</Link>
-    <Link href="/ex02">Cartão de Perfil</Link>
-    <Link href="/ex03">Botão</Link>
-    <Link href="/ex04">Lista de Produtos</Link>
-    <Link href="/ex05">Login</Link>
-    <Link href="/ex06">ScrollView</Link>
-    <Link href="/ex07">Ícones Apps</Link>
-    <Link href="/ex08">Perfil</Link>
-    <Link href="/ex09">Feed</Link>
-    <Link href="/ex10">Configurações</Link>
-
+    <View style={styles.container}>
+      <View style={styles.botaolink}>
+        <Link style={styles.link} href="/ex01">Layout Básico</Link>
+      </View>
+      <View style={styles.botaolink}>
+        <Link style={styles.link} href="/ex02">Cartão de Perfil</Link>
+      </View>
+      <View style={styles.botaolink}>
+        <Link style={styles.link} href="/ex03">Botão</Link>
+      </View>
+      <View style={styles.botaolink}>
+        <Link style={styles.link} href="/ex04">Lista de Produtos</Link>
+      </View>
+      <View style={styles.botaolink}>
+        <Link style={styles.link} href="/ex05">Login</Link>
+      </View>
+      <View style={styles.botaolink}>
+        <Link style={styles.link} href="/ex06">ScrollView</Link>
+      </View>
+      <View style={styles.botaolink}>
+        <Link style={styles.link} href="/ex07">Ícones Apps</Link>
+      </View>
+      <View style={styles.botaolink}>
+        <Link style={styles.link} href="/ex08">Perfil</Link>
+      </View>
+      <View style={styles.botaolink}>
+        <Link style={styles.link} href="/ex09">Feed</Link>
+      </View>
+      <View style={styles.botaolink}>
+        <Link style={styles.link} href="/ex10">Configurações</Link>
+      </View>
     </View>
+
+
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#4A0E5E", 
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  botaolink: {
+    width: "65%",
+    height: "auto",
+    backgroundColor: "#6A1B9A",
+    borderRadius: 15,
+    padding: 5,
+    marginVertical: 10,
+    alignItems: "center",
+    
+  },
+  link: {
+    fontSize: 18,
+    color: "#f0f0f0",
+  },
+});
+
